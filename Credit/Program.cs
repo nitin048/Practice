5a8ffe08504fae0585216c3d03640a24a671a7e4
nitin pradhan <nitinpradhan48@gmail.com>
2022-06-28 04:03:19 +0530

    commit

--- Credit/Program.cs
+++ Credit/Program.cs
@@ -1,12 +1,12 @@
 ﻿/* 
  Problem
-To access CRED programs, one needs to have a credit score of 750750 or more.
-Given that the present credit score is XX, determine if one can access CRED programs or not.
+To access CRED programs, one needs to have a credit score of 750 or more.
+Given that the present credit score is X, determine if one can access CRED programs or not.
 
 If it is possible to access CRED programs, output \texttt{YES}YES, otherwise output \texttt{NO}NO.
 
 Input Format
-The first and only line of input contains a single integer XX, the credit score.
+The first and only line of input contains a single integer X, the credit score.
 
 Output Format
 Print \texttt{YES}YES if it is possible to access CRED programs. Otherwise, print \texttt{NO}NO.
@@ -14,7 +14,7 @@ Print \texttt{YES}YES if it is possible to access CRED programs. Otherwise, prin
 You may print each character of the string in uppercase or lowercase (for example, the strings \texttt{YeS}YeS, \texttt{yEs}yEs, \texttt{yes}yes and \texttt{YES}YES will all be treated as identical).
 
 Constraints
-10000≤X≤1000
+0≤X≤100
 
 Subtasks
 Subtask 1 (100 points): Original constraints.
@@ -27,7 +27,19 @@ Output
 YES
 
 Explanation:
-Since 823 \geq 750823≥750, it is possible to access CRED programs.
+Since 823 ≥ 750, it is possible to access CRED programs.
+
+
+Sample 2:
+
+Input
+251
+
+Output
+NO
+
+Explanation:
+Since 251 <= 750, it is not possible to access CRED programs.
  */
 
 
@@ -39,7 +51,31 @@ namespace Credit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Cred Program");
+            Console.WriteLine("Check your credit score for the eligiblity \nie,min=600 max=750");
+            Console.WriteLine("Enter your credit score ");
+            int x = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (x > 750)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else if (x <= 600)
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("invalid input");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
 
         }
     }
