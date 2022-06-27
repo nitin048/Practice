0d09d792ad979a8568ecb6f124b430ef53b8eb61
nitin pradhan <nitinpradhan48@gmail.com>
2022-06-28 03:32:06 +0530

    initial commit

---
+++ Credit/Program.cs
@@ -0,0 +1,47 @@
+﻿/* 
+ Problem
+To access CRED programs, one needs to have a credit score of 750750 or more.
+Given that the present credit score is XX, determine if one can access CRED programs or not.
+
+If it is possible to access CRED programs, output \texttt{YES}YES, otherwise output \texttt{NO}NO.
+
+Input Format
+The first and only line of input contains a single integer XX, the credit score.
+
+Output Format
+Print \texttt{YES}YES if it is possible to access CRED programs. Otherwise, print \texttt{NO}NO.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings \texttt{YeS}YeS, \texttt{yEs}yEs, \texttt{yes}yes and \texttt{YES}YES will all be treated as identical).
+
+Constraints
+10000≤X≤1000
+
+Subtasks
+Subtask 1 (100 points): Original constraints.
+
+Sample 1:
+Input
+823
+
+Output
+YES
+
+Explanation:
+Since 823 \geq 750823≥750, it is possible to access CRED programs.
+ */
+
+
+using System;
+
+namespace Credit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
+
---
+++ Practice/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Problem
+Ezio can manipulate at most X number of guards in the apple garden.
+
+Given that there are Y number of guards, predict if he can safely manipulate all of them.
+
+Input Format
+First line will contain T, number of test cases. Then the test cases follow.
+Each test case contains of a single line of input, two integers X and Y.
+Output Format
+For each test case, print \text {YES}YES if it is possible for Ezio to manipulate all the guards.Otherwise, print \texttt{ NO}
+NO.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings \texttt{YeS}YeS, \texttt
+{ yEs}
+yEs, \texttt
+{ yes}
+yes and \texttt{YES}YES will all be treated as identical).
+
+Constraints
+
+1001≤T≤100
+1≤X,Y≤100
+
+
+ Input
+
+
+5 7
+6 6
+9 1
+
+Output
+NO
+YES
+YES
+ */
+
+
+using System;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the value of X");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the value of y");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x>=y)
+            {
+                Console.WriteLine("Yes");
+            }
+            else if (x<=y)
+            {
+                Console.WriteLine("No");
+            }
+            else if(x==y)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+        }
+    }
+}
+
