da7118f4cbffda98b9803ae19b208848e3738644
nitin pradhan <nitinpradhan48@gmail.com>
2022-06-28 23:55:30 +0530

    commit

--- CourseReg/Program.cs
+++ CourseReg/Program.cs
@@ -1,5 +1,16 @@
-﻿
-﻿using System;
+﻿/*
+ Problem
+There is a group of NN friends who wish to enroll in a course together. The course has a maximum capacity of MM students that can register for it. If there are KK other students who have already enrolled in the course, determine if it will still be possible for all the NN friends to do so or not.
+
+Input Format
+The first line contains a single integer TT - the number of test cases. Then the test cases follow.
+Each test case consists of a single line containing three integers NN, MM and KK - the size of the friend group, the capacity of the course and the number of students already registered for the course.
+Output Format
+For each test case, output Yes if it will be possible for all the NN friends to register for the course. Otherwise output No.
+
+You may print each character of Yes and No in uppercase or lowercase (for example, yes, yEs, YES will be considered identical).
+ */
+using System;
 
 namespace CourseReg
 {
