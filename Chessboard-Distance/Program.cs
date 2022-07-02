f8ae9184e7cbf58c639d4982202391d6a49d2af1
nitin pradhan <nitinpradhan48@gmail.com>
2022-07-02 11:24:21 +0530

    comiit

---
+++ Chessboard-Distance/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+ Problem
+https://www.codechef.com/submit/CHESSDIST
+ */
+using System;
+
+namespace Chessboard_Distance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string confrim;
+                do
+                {
+                    Console.WriteLine("Let's find chessboard distance♖♘♟");
+                    Console.WriteLine("Enter the values of X");
+                    int[]x = new int[2];
+                    int[] y = new int[2];
+                    for (int i = 0; i < 2; i++)
+                    {
+                        x[i] = int.Parse(Console.ReadLine());
+                    } 
+                    Console.WriteLine("Enter the value of Y");
+                    for (int j = 0; j < 2; j++)
+                    {
+                        y[j] = int.Parse(Console.ReadLine());
+                    }
+                    int x1 = x[0];
+                    int x2 = x[1];
+                    int y1 = y[0];
+                    int y2 = y[1];
+
+                     int s =Math.Abs(x1 - y1);
+                    int p = Math.Abs(y2 - x2);
+
+                    if (s>=p)
+                    {
+                        Console.WriteLine("chessboard distance :" +" "+s);
+                    }
+                    else
+                    {
+                        Console.WriteLine("chessboard distance :" + " " + p);
+                    }
+
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("Do you want to run the prgm again?🤔\nYes or No");
+                    confrim = Console.ReadLine().ToLower();
+                } while (confrim=="yes");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Thank you!!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input!!!☠️");
+                Console.WriteLine("\nHere is the exception:->" + " " + ex);
+            }
+        }
+    }
+}
+
---
+++ Finding-Square-Roots/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+
+Problem
+
+In olden days finding square roots seemed to be difficult but nowadays it can be easily done using in-built functions available across many languages .
+
+Assume that you happen to hear the above words and you want to give a try in finding the square root of any given integer using in-built functions. So here's your chance.
+
+Input
+The first line of the input contains an integer T, the number of test cases. T lines follow. Each line contains an integer N whose square root needs to be computed.
+
+Output
+For each line of the input, output the square root of the input integer, rounded down to the nearest integer, in a new line.
+
+Input:
+
+10
+5
+10000
+
+Output:
+3
+2
+100
+
+ */
+using System;
+
+namespace Finding_Square_Roots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string confrim;
+                do
+                {
+                    Console.WriteLine("------------------------------------------");
+                    Console.WriteLine("Hello,Let's find the Find the Sqrt🤓");
+                    uint x = uint.Parse(Console.ReadLine());
+                    double ans = Math.Sqrt(x);
+                    Console.WriteLine("The sqrt of "+x+" is :"+" "+Math.Round(ans));
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("Do you want to run the prgm again?🤔\nYes or No");
+                    confrim = Console.ReadLine().ToLower();
+                } while (confrim=="yes");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Thank you!!🤗");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input!!!☠️");
+                Console.WriteLine("\nHere is the exception:->"+" "+ex);
+            }
+            
+        }
+    }
+}
+
