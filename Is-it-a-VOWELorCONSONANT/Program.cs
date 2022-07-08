65dcc71572b67e7221248e2befbd34afd02460c7
nitin pradhan <nitinpradhan48@gmail.com>
2022-07-08 23:44:45 +0530

    comiit

---
+++ Is-it-a-VOWELorCONSONANT/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+ https://www.codechef.com/submit/VOWELTB
+ */
+using System;
+
+namespace Is_it_a_VOWELorCONSONANT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string confrim;
+                do
+                {
+                    Console.WriteLine("Let's check Is_it_a_VOWEL or CONSONANT ");
+                    Console.WriteLine("Enter a character (C) as input and check whether the given character is a vowel or a consonant.");
+                    string x = Console.ReadLine().ToLower();
+                    Console.WriteLine("-------------------------------------------------------------");
+                    if ((x=="a")||x=="e"||(x=="i")||(x=="o")||(x=="u"))
+                    {
+                        Console.WriteLine("VOWEL");
+                    }
+                    else
+                    {
+                        Console.WriteLine("CONSONANT");
+                    }
+                    Console.WriteLine("--------------------------------------------------------------");
+                    Console.WriteLine("Do you want to run the prgm again?🤔\nYes or No");
+                    confrim = Console.ReadLine().ToLower();
+                } while (confrim == "yes");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Thank you!!👋");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input!!!☠️");
+                Console.WriteLine("\nHere is the exception:->" + " " + ex);
+            }
+        }
+    }
+}
+
---
+++ Which-Mixture/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ https://www.codechef.com/submit/MIXTURE
+ */
+using System;
+
+namespace Which_Mixture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string confrim;
+                do
+                {
+                    Console.WriteLine("Let's check Which_Mixture it is ");
+                    Console.WriteLine("Enter an input");
+                    int[] x = new int[2];
+                    for (int i = 0; i < x.Length; i++)
+                    {
+                        x[i] = int.Parse(Console.ReadLine());
+                    }
+                    Console.WriteLine("-------------------------------------------------------------");
+                    if ((x[0]>0)&&(x[1]>0))
+                    {
+                        Console.WriteLine("Solution");
+                    }
+                    else if((x[1]==0)&&(x[0]>0))
+                    {
+                        Console.WriteLine("Solid");
+                    }
+                    else if ((x[0]==0)&&(x[1]>0))
+                    {
+                        Console.WriteLine("Liquid");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
+                
+                  
+                    Console.WriteLine("--------------------------------------------------------------");
+                    Console.WriteLine("Do you want to run the prgm again?🤔\nYes or No");
+                    confrim = Console.ReadLine().ToLower();
+                } while (confrim == "yes");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Thank you!!👋");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input!!!☠️");
+                Console.WriteLine("\nHere is the exception:->" + " " + ex);
+            }
+        }
+    }
+}
+
