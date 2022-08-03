83f13044ceca4c0ec234c37b02d5c35ac10a2194
nitin pradhan <nitinpradhan48@gmail.com>
2022-08-03 22:47:32 +0530

    commit

---
+++ N-Buttons-1-Bulb/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace N_Buttons_1_Bulb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string confrim;
+                do
+                {
+                    System.Console.WriteLine("Enter your data");
+                    byte t = Byte.Parse(Console.ReadLine());
+
+                    for (byte i = 0; i < t; i++)
+                    {
+
+                        byte counter = 0;
+
+                        byte n = Byte.Parse(Console.ReadLine());
+                        char[] s = Console.ReadLine().ToCharArray();
+                        char[] r = Console.ReadLine().ToCharArray();
+
+                        for (byte o = 0; o < n; o++)
+                        {
+                            if (r[o] != s[o])
+                                counter++;
+                        }
+
+                        Console.WriteLine(counter % 2 == 0 ? '1' : '0');
+                    }
+
+                    Console.WriteLine("--------------------------------------------------------------");
+                    Console.WriteLine("Do you want to run the prgm again?🤔\nYes or No");
+                    confrim = Console.ReadLine().ToLower();
+                } while (confrim == "yes");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Thank you!!👋");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input!!!☠️");
+                Console.WriteLine("\nHere is the exception:->" + " " + ex);
+            }
+        }
+    }
+}
+
