a1aac01ef34a573b5603d0865bc7cc80be02f5bb
nitin pradhan <nitinpradhan48@gmail.com>
2022-08-09 11:50:50 +0530

    comit

---
+++ Bidding/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Bidding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string confrim;
+                do
+                {
+                    System.Console.WriteLine("Enter your data");
+                    int x = Convert.ToInt32(Console.ReadLine()), x1 = 0, x2 = 0, x3 = 0;
+
+                    for (int i = 0; i < x; ++i)
+                    {
+                        string[] split = Console.ReadLine().Split(' ');
+                        x1 = Convert.ToInt32(split[0]);
+                        x2 = Convert.ToInt32(split[1]);
+                        x3 = Convert.ToInt32(split[2]);
+
+                        
+                        if ((x1>x2) && (x1>x3))
+                        {
+                            Console.WriteLine("Alice");
+                        }
+
+                        else if ((x2 > x1) && (x2 > x3))
+                        {
+                            Console.WriteLine("Bob");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Charlie");
+                        }
+                    }
+
+                    Console.WriteLine("--------------------------------------------------------------");
+                    Console.WriteLine("Do you want to run the prgm again?🤔\nYes or No");
+                    confrim = Console.ReadLine().ToLower();
+                } while (confrim == "yes");
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Thank you!!👋");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid input!!!☠️");
+                Console.WriteLine("\nHere is the exception:->" + " " + ex);
+            }
+        }
+    }
+}
+
